43eb7b5900a1d56bd3c3093df5eef7f99e2cbff1
Annie Tsang <annietsang23@gmail.com>
2019-02-19 11:33:24 +0000

    updated codes for creating tables and dummy figures

--- dummy.sql
+++ dummy.sql
@@ -13,24 +13,24 @@ CREATE TABLE IF NOT EXISTS Product
     product_description TEXT,
     price NUMERIC,
     quantity INTEGER,
-    categoryID VARCHAR(2),
-    conditionID VARCHAR(40),
-    sellerID VARCHAR(40),
+    categoryID INTEGER,
+    conditionID INTEGER,
+    sellerID INTEGER,
     auctionable BOOLEAN,
-    enddate VARCHAR(40),
+    enddate VARCHAR(40)
 ) 
 ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS Category
 (
-    categoryID VARCHAR(2) PRIMARY KEY,
+    categoryID INTEGER AUTO_INCREMENT PRIMARY KEY,
     categoryname VARCHAR(40) NOT NULL
 ) 
 ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS ConditionIndex
 (
-    conditionID VARCHAR(2) PRIMARY KEY,
+    conditionID INTEGER AUTO_INCREMENT PRIMARY KEY,
     conditionname VARCHAR(40) NOT NULL
 ) 
 ENGINE = InnoDB;
@@ -38,15 +38,60 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS Archive
 (
     archiveID INTEGER AUTO_INCREMENT PRIMARY KEY,
-    productID VARCHAR(11) NOT NULL,
+    productID INTEGER NOT NULL,
     product_description TEXT NOT NULL,
     dealprice NUMERIC NOT NULL,
     quantity INTEGER NOT NULL,
-    categoryID VARCHAR(2) NOT NULL,
-    conditionID VARCHAR(40) NOT NULL,
-    buyerID VARCHAR(40) NOT NULL,
-    sellerID VARCHAR(40) NOT NULL,
+    categoryID INTEGER NOT NULL,
+    conditionID INTEGER NOT NULL,
+    buyerID INTEGER NOT NULL,
+    sellerID INTEGER NOT NULL,
     auctionable BOOLEAN NOT NULL,
     dealdate VARCHAR(40) NOT NULL
 ) 
 ENGINE = InnoDB;
+
+INSERT INTO Category (categoryname)
+VALUES ('Electronics');
+
+INSERT INTO Category (categoryname)
+VALUES ('Food');
+
+INSERT INTO Category (categoryname)
+VALUES ('Fashion');
+
+INSERT INTO Category (categoryname)
+VALUES ('Home');
+
+INSERT INTO Category (categoryname)
+VALUES ('Health & Beauty');
+
+INSERT INTO Category (categoryname)
+VALUES ('Sports');
+
+INSERT INTO Category (categoryname)
+VALUES ('Toys & Games');
+
+INSERT INTO Category (categoryname)
+VALUES ('Art & Music');
+
+INSERT INTO Category (categoryname)
+VALUES ('Miscellaneous');
+
+INSERT INTO ConditionIndex (conditionname)
+VALUES ('New');
+
+INSERT INTO ConditionIndex (conditionname)
+VALUES ('Refurbished');
+
+INSERT INTO ConditionIndex (conditionname)
+VALUES ('Used / Worn');
+
+INSERT INTO Archive (productID,product_description,dealprice,quantity,categoryID,conditionID,buyerID,sellerID,auctionable,dealdate)
+VALUES (1,'macbook pro',1300,4,1,1,16,1,0,'2019-01-15');
+
+INSERT INTO Archive (productID,product_description,dealprice,quantity,categoryID,conditionID,buyerID,sellerID,auctionable,dealdate)
+VALUES (2,'macbook pro',1300,1,1,1,16,1,0,'2019-01-10');
+
+INSERT INTO Archive (productID,product_description,dealprice,quantity,categoryID,conditionID,buyerID,sellerID,auctionable,dealdate)
+VALUES (3,'macbook pro',815,1,1,3,16,2,1,'2019-01-20');
