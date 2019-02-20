43339bae66cdce51113fd7ba6e1c27763601abf5
sergibot <33570931+sergibot@users.noreply.github.com>
2019-02-20 17:15:18 +0000

    adding basic html links to index.php
    and removing old help-with-git txt file (which just had links to sites in)

--- index.php
+++ index.php
@@ -2,9 +2,16 @@
 <html>
 <body>
 
-<h1>hello worlds PHP page</h1>
+<h1>hello world! index.php PHP page</h1>
+<p>
+    <a href="listing.php" title="placeholder mouseover text">
+        html link to Listing.php
+    </a>
+</p>
 
 <?php
+echo "echoing from php\n";
+print "printing from php\n";
 echo "hello world 5";
 echo "hello world hj";
 echo "hello world dk";
