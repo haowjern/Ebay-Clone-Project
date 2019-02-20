2bdbf4fdbf1f5224f0cb2932174d82511fc3697f
sergibot <33570931+sergibot@users.noreply.github.com>
2019-02-20 17:22:52 +0000

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,14 @@
 
 <h1>hello world! index.php PHP page</h1>
 <p>
+    <a href="database.php" title="placeholder mouseover text">
+        html link to Database.php
+    </a>
     <a href="listing.php" title="placeholder mouseover text">
         html link to Listing.php
+    </a>
+    <a href="selectitem.php" title="placeholder mouseover text">
+        html link to SelectItem.php
     </a>
 </p>
 
