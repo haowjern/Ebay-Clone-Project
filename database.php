105273e5f91fa957f0c51a910a5982c772b0deee
sergibot <33570931+sergibot@users.noreply.github.com>
2019-02-21 16:52:02 +0000

    fiddling w db conn

--- database.php
+++ database.php
@@ -5,7 +5,10 @@
 <p>Connect to Dummy database</p>
  
   <?php
-  $connection = mysqli_connect("localhost", "ebaydb");
+  #$connection = mysqli_connect("localhost", "ebaydb");
+  
+  $connection = mysqli_connect("https://ebaydatabasegithub.azurewebsites.net/", "ebaydb");
+  
 
   if (mysqli_connect_errno()){
     echo 'Failed to connect to the MySQL server: '. mysqli_connect_error();}
