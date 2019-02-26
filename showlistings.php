f7e24dff7b47dfc00d09fcb6ddffacfc4ffd6ff6
sergibot <33570931+sergibot@users.noreply.github.com>
2019-02-26 13:06:54 +0000

    basic showlistings.php

--- showlistings.php
+++ showlistings.php
@@ -1,16 +1,25 @@
-<!DOCTYPE html>
-<html>
-<body>
-<p>
-<h1>
-Showing Listings
-</h1>
-</p>
+<?php include 'database.php';?>
 
 <?php
 
-echo "php here"
+echo "Listings shown below: <br>";
 
+$sql="SELECT * FROM Product;
+$result=$connection->query($sql);
+
+if ($result->num_rows>0){
+    #create table header
+    echo "<table><tr><th>product ID</th><th>product details</th><th>price</th></tr>";
+    //output data of each row in table
+    while($row=$result->fetch_assoc()){
+        echo "<tr><td>". $row["productID"]."</td><td>".$row["product_description"] . "</td><td>".$row["price"]. "</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "no result found";
+}
+
+$connection->close();
 
 
 
