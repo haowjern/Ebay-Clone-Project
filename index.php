38bdde6b86c7aab31bd93c84546986baa87ef75e
sergibot <33570931+sergibot@users.noreply.github.com>
2019-02-26 12:34:57 +0000

    recover index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>hello world! index.php PHP page</h1>
+<p>
+    <a href="database.php" title="placeholder mouseover text">
+        html link to Database.php
+    </a>
+    <a href="listing.php" title="placeholder mouseover text">
+        html link to Listing.php
+    </a>
+    <a href="selectitem.php" title="placeholder mouseover text">
+        html link to SelectItem.php
+    </a>
+
+    <?php
+    echo "hello world from php"
+    ?>
+</p>
+</body>
+</html>
