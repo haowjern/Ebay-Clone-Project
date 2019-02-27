886604b85bc3ce99f831cf9ea612d4b4e7168e89
annietsang23 <44124835+annietsang23@users.noreply.github.com>
2019-02-27 21:49:06 +0000

    Update index.php

--- index.php
+++ index.php
@@ -5,21 +5,28 @@
 <p>
     <a href="database.php" title="placeholder mouseover text">
         html link to database.php
+    <br>
     </a>
+    <p>seller activity</p><br>
     <a href="/activity/editlisting.php" title="placeholder mouseover text">
-        html link to editlisting.php
+        html link to activity/editlisting.php
+    <br>
     </a>
     <a href="/activity/sellershop.php" title="placeholder mouseover text">
         html link to /activity/sellershop.php
+    <br>
     </a>
     <a href="/activity/sellinghistory.php" title="placeholder mouseover text">
         html link to /activity/sellinghistory.php
+    <br>
     </a>
     <a href="selectitem.php" title="placeholder mouseover text">
         html link to selectitem.php
+    <br>
     </a>
     <a href="showlistings.php" title="placeholder mouseover text">
         html link to showlistings.php
+    <br>
     </a>
 
     <?php
