2e50f058924d5e3acc10b7c681009726d7c1e725
Annie Tsang <annietsang23@gmail.com>
2019-02-27 19:42:38 +0000

    changed logic to enable reusability

--- removelisting.php
+++ removelisting.php
@@ -4,33 +4,40 @@ session_start();
 //script to remove listing from product table.
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    include "database.php";
-
     $productID=mysqli_real_escape_string($connection,$_POST["productID"]);
 
-    $sql="DELETE FROM Product WHERE productID='$productID'";
-            $result=$connection->query($sql);
+    include "database.php";
 
-            if ($result==TRUE){
-                echo "Record deleted successfully";
+}else{
+    
+    $productID=mysqli_real_escape_string($connection,$_SESSION["remove_productID"]);
+}
 
-            } else {
-            echo "Error: ". $sql . "<br>" . $connection->error;
-            }
+$sql="DELETE FROM Product WHERE productID='$productID'";
+        $result=$connection->query($sql);
+
+        if ($result==TRUE){
+            echo "Record deleted successfully";
+
+        } else {
+        echo "Error: ". $sql . "<br>" . $connection->error;
+        }
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $connection->close();
-    header("Location: sellershop.php"); 
 
-}
     $host  = $_SERVER['HTTP_HOST'];
     $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
     $extra = 'sellershop.php';
     $link="http://".$host.$uri."/".$extra;
 
-//                 header("Location: http://$host$uri/$extra");
+    header("Location: http://$host$uri/$extra");
+}
 ?>
 <html>
 <body>
-    <button onclick="window.location.href = '<?php echo $link; ?>'">Go back to myshop</button>
+    <!-- <button onclick="window.location.href = '<?php echo $link; ?>'">Go back to myshop</button> -->
 </body>
 </html>
