a1f32480c17713854add45319cae86dd589bf00e
Annie Tsang <annietsang23@gmail.com>
2019-02-27 18:14:10 +0000

    changed table layout and added buttons for bid event

--- sellershop.php
+++ sellershop.php
@@ -9,6 +9,11 @@ if ($count==0){
     echo "no result found";
 }
 
+$host  = $_SERVER['HTTP_HOST'];
+$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+$extra = 'editlisting.php';
+$link_newlisting="http://".$host.$uri."/".$extra;
+
 ?>
 
 <html>
@@ -16,17 +21,18 @@ if ($count==0){
 </head>
 
 <body>
-
-
 <p>My Shop</p>
+<button onclick="window.location.href = '<?php echo $link_newlisting; ?>'">Create new listing</button>
 <p id="t"></p>
 
+
 <!-- create table header -->
-<table id=active_listing_table width="1000">
+<table id=active_listing_table width="1200">
     <tr>
         <th>Image</th>
-        <th>Product details</th>
-        <th>Listing ends on: </th>
+        <th>Product Details</th>
+        <th>Start (Reserve) Price (£)</th>
+        <th>Listing ends on </th>
         <th>Auctionable?</th>
         <th>Action</th>
         <th>listing created on</th>
@@ -37,50 +43,74 @@ if ($count==0){
 var count="<?php echo $count?>";
 document.getElementById("t").innerHTML = "No. of active listings: "+count;
 
+//copy the php array into javascript array
+var each_listing=<?php echo json_encode($_SESSION['all_active_listings'],JSON_PRETTY_PRINT)?>;
+
+<?php unset($_SESSION["all_active_listings"]);?>;
+
 for (i=0;i<count;i++){
     //for each active listing, create a row in the table.
     var table=document.getElementById("active_listing_table");
     var row=table.insertRow(-1);
     var cell_image=row.insertCell(0);
     var cell_details=row.insertCell(1);
-    var cell_enddate=row.insertCell(2);
-    var cell_auctionable=row.insertCell(3);
-    var cell_action=row.insertCell(4);
+    var cell_price=row.insertCell(2);
+    var cell_enddate=row.insertCell(3);
+    var cell_auctionable=row.insertCell(4);
+    var cell_action=row.insertCell(5);
  
-    //insert image iin the 1st column
+    //insert image iin the 1st column (image)
     cell_image.style.textAlign="center";
     cell_image.innerHTML="(image)";
 
-    //insert product details into the 2nd column
+    //insert product details into the 2nd column (Details)
     cell_details.style.textAlign="center";
 
-
-    //copy the php array into javascript array
-    var each_listing=<?php echo json_encode($_SESSION['all_active_listings'],JSON_PRETTY_PRINT)?>;
-
-
     cell_details.innerHTML=each_listing[i]["product_description"]+
-                                "<br> price: "+each_listing[i]["price"]+
                                 "<br>quantity: "+each_listing[i]["quantity"]+
                                 "<br>category: "+each_listing[i]["categoryname"]+
                                 "<br>condition: "+each_listing[i]["conditionname"]+"<br><br>";
 
-    //insert listing enddate into the 3rd column
+    //insert start(reserve) price into the 3rd column (start/reserve price)
+    cell_price.style.textAlign="center";
+    cell_price.innerHTML=each_listing[i]["price"];
+
+    //insert listing enddate into the 4th column (listing enddate)
     cell_enddate.style.textAlign="center";
     cell_enddate.innerHTML=each_listing[i]["enddate"];
 
-    //insert listing auction status into the 4th column
+    //insert listing auction status into the 5th column (auction)
     cell_auctionable.style.textAlign="center";
 
     if (each_listing[i]["auctionable"]=="0"){
-        each_listing[i]["auctionable"]="No";}
-        else{
-            each_listing[i]["auctionable"]="Yes";}
+        each_listing[i]["auctionable"]="No";
+        cell_auctionable.innerHTML="No";
+        }else{
+            each_listing[i]["auctionable"]="Yes";
 
-    cell_auctionable.innerHTML=each_listing[i]["auctionable"];
-    
+            //create the form to go to bid event if it is auctionable
+            var fm_bid=document.createElement('form');
+            //name the form with productID
+            fm_bid.setAttribute("name","form_bid"+each_listing[i]["productID"]);
+            fm_bid.setAttribute("method","post");
+            fm_bid.setAttribute("action","");
 
-    //insert forms with buttons in 5th column
+            var hiddenField_bid=document.createElement("input");
+                hiddenField_bid.setAttribute("type","hidden");
+                hiddenField_bid.setAttribute("name","productID");
+                hiddenField_bid.setAttribute("value",each_listing[i]["productID"]);
+
+                fm_bid.appendChild(hiddenField_bid);
+
+            var bid_button=document.createElement("input");
+            bid_button.setAttribute("type","submit");
+            bid_button.setAttribute("value","go to bid event");
+        
+            fm_bid.appendChild(bid_button);
+            cell_auctionable.appendChild(fm_bid);
+        }
+
+    //insert forms with buttons in 6th column (action)
     cell_action.style.textAlign="center";
 
     //create the form to edit item
@@ -112,8 +142,8 @@ for (i=0;i<count;i++){
     //name the form with productID
     fm_remove.setAttribute("name","form_remove"+each_listing[i]["productID"]);
     fm_remove.setAttribute("method","post");
-    fm_remove.setAttribute("action","testing1.php");
-    // fm_remove.setAttribute("onsubmit","alert('confirm delete!');return false")
+    fm_remove.setAttribute("action","removelisting.php");
+
 
     var hiddenField_remove=document.createElement("input");
         hiddenField_remove.setAttribute("type","hidden");
@@ -125,6 +155,7 @@ for (i=0;i<count;i++){
     var remove_button=document.createElement("input");
     remove_button.setAttribute("type","submit");
     remove_button.setAttribute("value","remove");
+    //alert pop up prior to deletion
     remove_button.setAttribute("onclick","return warning_msg();");
     fm_remove.appendChild(remove_button);
     cell_action.appendChild(fm_remove);
@@ -134,10 +165,6 @@ for (i=0;i<count;i++){
 
 function warning_msg(){
     return confirm("confirm remove this listing?");
-        // var confirm_remove=confirm("Confirm removing this item?");
-        // if (confirm_remove){
-        //     f.submit();
-        // }
     }
 
 </script>
