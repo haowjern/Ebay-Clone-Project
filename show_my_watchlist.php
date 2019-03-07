37ef398231a4c6572a817cafd36f258300f0657c
sergibot <33570931+sergibot@users.noreply.github.com>
2019-03-07 12:30:53 +0000

    made show_watchlist table queries

--- show_my_watchlist.php
+++ show_my_watchlist.php
@@ -6,7 +6,7 @@ $_SESSION['userID'] = 11;
  
 $_SESSION['current_user'] = $_SESSION['userID']; // set the current user to be the buyer 
     
-show_watchlist($_SESSION['current_user'], "show");  
+show_watchlist($_SESSION['current_user']);  
 ?>
 
 
