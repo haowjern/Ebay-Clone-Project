c8c46b7e8fe954dce967400330960705b56ca736
Annie Tsang <annietsang23@gmail.com>
2019-03-12 17:43:14 +0000

    fixed sql and added cron job

--- activity/addarchive.php
+++ activity/addarchive.php
@@ -42,6 +42,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $sql="UPDATE Product 
                     SET quantity='$inventory_new'
                     WHERE productID=$productID";
+            $connection->query($sql);
         }
 
         //insert item in archive table
