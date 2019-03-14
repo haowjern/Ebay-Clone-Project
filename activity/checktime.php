e282cafec3a1cad5d2f3063b2c749649ccb12bd9
sergibot <33570931+sergibot@users.noreply.github.com>
2019-03-14 08:09:43 +0000

    emails bidders on newbid, starting checktime email

--- activity/checktime.php
+++ activity/checktime.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-
+include 'email_sellers_autobidextend.php';
 //this script runs every hour (from cron-jobs.txt) to search for listings that are expiring
 
 $connection = mysqli_connect("localhost", "root", "", "ebaydb");
@@ -11,7 +11,7 @@ $_SESSION["expired_listings"]=array();
 $t=ltrim(date("H",time()),"0").":00:00";
 $today=date("Y-m-d"); 
 
-//select all the produc that expires at current hour today
+//select all the products that expire at current hour today
 
 $sql="SELECT p.product_name,p.product_description,p.startdate,p.enddate,p.endtime,u.username,u.email
         FROM Product p,Users u
@@ -49,6 +49,8 @@ if ($result->num_rows>0){
         WHERE productID=$productID";
 
         $connection->query($sql);
+
+        //send_email_updating_sellers(...)
     }
 
  //send email to the sellers involved, and notify them that the expiry date is now extended by a month by default. They can log in to view/change the date.
