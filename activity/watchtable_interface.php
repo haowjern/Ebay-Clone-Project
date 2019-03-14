53d8bbf5351f65726fbb992cee687fefc54d39c2
sergibot <33570931+sergibot@users.noreply.github.com>
2019-03-14 10:50:36 +0000

    FIX HEADER47 username and tidying all watch

--- activity/watchtable_interface.php
+++ activity/watchtable_interface.php
@@ -8,11 +8,9 @@ function get_watchtable($buyerID) {
         $sql = "SELECT * FROM Watchlist WHERE buyerID = '$buyerID'
                 ORDER BY watchID DESC";
         $result = $connection->query($sql);
-        
         if ($result->num_rows>0) { 
             while ($row=$result->fetch_assoc()) {
                 $watch_arr['watchID'] = $row['watchID'];
-                //$watch_arr['productID'] = $row['productID'];
                 $productID = $row['productID'];
                 $watch_arr['buyerID'] = $row['buyerID'];
 
@@ -23,36 +21,26 @@ function get_watchtable($buyerID) {
                     $watch_arr['productName'] = $row['product_name'];
                     $watch_arr['endDate'] = $row['enddate'];
                     $watch_arr['endTime'] = $row['endtime'];
-                    //$watch_arr['sellerID'] = $row['sellerID'];
 
+                    // get seller name from seller id and user table
                     $sellerID = $row['sellerID'];
                     $sql = "SELECT * FROM Users WHERE userID = '$sellerID'";
-                    $result1 = $connection->query($sql);
-                    while ($row=$result1->fetch_assoc()) {
+                    $result2 = $connection->query($sql);
+                    while ($row=$result2->fetch_assoc()) {
                         $watch_arr['sellerName'] = $row['username'];
                     }
-                
                 }
                 
-
-                // if there's time, do the following:
-                // from bidevents, via productid:
-                // get latest bid matching productid
-                    // get bidprice from latest bid    
-                    // get buyerid from latest bid
-                        // get buyer name from users
-
-
                 array_push($watches, $watch_arr);
             }
             //echo("Received watchlist item successfully.");
         } else {
             // does this work fine? test...
 
-            $watch_arr['productName'] = 0;
-            $watch_arr['sellerName'] = 0; 
-            $watch_arr['endDate'] = 0;
-            $watch_arr['endTime'] = 0;
+            $watch_arr['productName'] = "None";
+            $watch_arr['sellerName'] = "-"; 
+            $watch_arr['endDate'] = "-";
+            $watch_arr['endTime'] = "-";
                     
             array_push($watches, $watch_arr);
         }
