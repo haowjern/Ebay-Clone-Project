e05b4972175567f0d5c2b0e52c3372588b711835
sergibot <33570931+sergibot@users.noreply.github.com>
2019-03-14 16:22:22 +0000

    dlt file and gaps, un-debug send_email

--- activity/watchlist_table.php
+++ activity/watchlist_table.php
@@ -2,12 +2,9 @@
     include_once("watchtable_interface.php");
     include '../database.php';
     $buyerID = $_SESSION['userID'];
-
     $watches = get_watchtable($buyerID); // get all watch elements for user
     
     foreach ($watches as $watch) {
-
         echo ("<tr> <td> ".$watch["productName"]."</td> <td> ".$watch["sellerName"]."</td> <td> ".$watch["endDate"]."</td> <td> ".$watch["endTime"]."</td> </tr>");
-
     }
 ?> 
