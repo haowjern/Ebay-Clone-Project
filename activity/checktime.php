e050db2e8410cb2a04cbff8d2d59b83665918417
haowjern <t.haowjern@gmail.com>
2019-03-15 01:41:32 +0000

    checktime fixed

--- activity/checktime.php
+++ activity/checktime.php
@@ -4,9 +4,9 @@ include 'email_sellers_autobidextend.php';
 include 'send_email.php';
 //this script runs every hour (from cron-jobs.txt) to search for listings that are expiring
 
-$myfile = fopen("/Applications/MAMP/anniebranch/ebay-database-system-project/cron-test.txt", "w");
+//$myfile = fopen("/Applications/MAMP/anniebranch/ebay-database-system-project/cron-test.txt", "w");
 
-fwrite($myfile, mktime());
+//fwrite($myfile, mktime());
 
 $connection = mysqli_connect("localhost", "root", "", "ebaydb");
 
@@ -19,13 +19,13 @@ if ($t!="00"){
 } else{
     $t=$t.":00:00";
 }
+
 $today=new DateTime();
 $today_str = $today->format('Y-m-d');
 //select all the products (non-bidding) that expires at current hour today
 $sql="SELECT p.productID, p.product_name,p.product_description,p.startdate,p.enddate,p.endtime,u.username,u.email
         FROM Product p,Users u
         WHERE p.sellerID=u.userID AND p.auctionable=0 AND p.enddate='$today_str' AND endtime='$t'" ;
-
 
 $result=$connection->query($sql);
 
@@ -54,7 +54,6 @@ if ($result->num_rows>0){
         $seller_email = $value["email"];
         //update the enddate in product table per productID
         $productID=$value["productID"];
-        echo $productID;
         $sql="UPDATE Product 
         SET enddate='$newdate'
         WHERE productID=$productID";
@@ -66,111 +65,111 @@ if ($result->num_rows>0){
 // //select all the products (bidding) that expires at current hour today
 
 
-// $sql="SELECT p.productID, p.product_name,p.product_description,p.quantity, p.categoryID, p.conditionID, p.sellerID, p.auctionable, p.startdate,p.enddate,p.endtime,b.buyerID
-//         FROM Product p,BidEvents b
-//         WHERE p.auctionable=1";
-// $result_all=$connection->query($sql);
-// if ($result_all->num_rows>0){
-//     while ($value = $result_all->fetch_assoc()) {
-//         $productID = $value["productID"];
-//         $product_name = $value["product_name"];
-//         $product_description = $value["product_description"];
-//         $quantity = $value["quantity"];
-//         $categoryID = $value["categoryID"];
-//         $conditionID = $value["conditionID"];
-//         $sellerID = $value["sellerID"];
-//         $auctionable = $value["auctionable"];
-//         $dealdate = $value["enddate"]; // no buyer comment and seller comment
-//         $enddate = $value["enddate"];
-//         $endtime = $value["endtime"];
+$sql="SELECT p.productID, p.product_name,p.product_description,p.quantity, p.categoryID, p.conditionID, p.sellerID, p.auctionable, p.startdate,p.enddate,p.endtime,b.buyerID
+        FROM Product p,BidEvents b
+        WHERE p.auctionable=1";
+$result_all=$connection->query($sql);
+if ($result_all->num_rows>0){
+    while ($value = $result_all->fetch_assoc()) {
+        $productID = $value["productID"];
+        $product_name = $value["product_name"];
+        $product_description = $value["product_description"];
+        $quantity = $value["quantity"];
+        $categoryID = $value["categoryID"];
+        $conditionID = $value["conditionID"];
+        $sellerID = $value["sellerID"];
+        $auctionable = $value["auctionable"];
+        $dealdate = $value["enddate"]; // no buyer comment and seller comment
+        $enddate = $value["enddate"];
+        $endtime = $value["endtime"];
         
-//         // first check if buyer enddate has ended
-//         if ($today >= new DateTime($enddate." ".$endtime)) {
-//             $sql = "SELECT * FROM BidEvents 
-//                 WHERE productID = '$productID'
-//                 ORDER BY bidPrice DESC";
-//             $result = $connection->query($sql);
-//             if ($result->num_rows>0) {
-//                 // get the highest bid payer
-//                 while ($row = $result->fetch_assoc()) {
-//                     $buyerID = $row["buyerID"];
-//                     $payment = $row["payment"];
-//                     $bidPrice = $row["bidPrice"];
-//                     $bidDate = $row["bidDate"];
-//                     $bidTime = $row["bidTime"]; 
-//                     $dealprice = $bidPrice; 
-//                     //get the user's email address
-//                     $sql = "SELECT username, email, accountbalance FROM Users WHERE userID = $buyerID"; 
-//                     $result_user=$connection->query($sql); 
-//                     // if found
-//                     if ($result_user->num_rows>0) {
-//                         while ($row_user=$result_user->fetch_assoc()) {
-//                             $buyer_name = $row_user["username"];
-//                             $buyer_email = $row_user["email"];
-//                             $accountbalance = $row_user["accountbalance"];
-//                             // if there is enough in the bank account
-//                             if ($accountbalance >= $bidPrice) {
-//                                 $subject = "Congratulations! You have won the auction for '$product_name'!";
-//                                 $body = "Description: ".$product_description;
-//                                 $altbody = $body;
-//                                 $emailee_name = "DatabaseCW";
-//                                 send_to_email($buyer_email, $subject, $body, $altbody, $emailee_name);
+        // first check if buyer enddate has ended
+        if ($today >= new DateTime($enddate." ".$endtime)) {
+            $sql = "SELECT * FROM BidEvents 
+                WHERE productID = '$productID'
+                ORDER BY bidPrice DESC";
+            $result = $connection->query($sql);
+            if ($result->num_rows>0) {
+                // get the highest bid payer
+                while ($row = $result->fetch_assoc()) {
+                    $buyerID = $row["buyerID"];
+                    $payment = $row["payment"];
+                    $bidPrice = $row["bidPrice"];
+                    $bidDate = $row["bidDate"];
+                    $bidTime = $row["bidTime"]; 
+                    $dealprice = $bidPrice; 
+                    //get the user's email address
+                    $sql = "SELECT username, email, accountbalance FROM Users WHERE userID = $buyerID"; 
+                    $result_user=$connection->query($sql); 
+                    // if found
+                    if ($result_user->num_rows>0) {
+                        while ($row_user=$result_user->fetch_assoc()) {
+                            $buyer_name = $row_user["username"];
+                            $buyer_email = $row_user["email"];
+                            $accountbalance = $row_user["accountbalance"];
+                            // if there is enough in the bank account
+                            if ($accountbalance >= $bidPrice) {
+                                $subject = "Congratulations! You have won the auction for '$product_name'!";
+                                $body = "Description: ".$product_description;
+                                $altbody = $body;
+                                $emailee_name = "DatabaseCW";
+                                send_to_email($buyer_email, $subject, $body, $altbody, $emailee_name);
             
-//                                 // deduct balance from user
-//                                 $remainingbalance = $accountbalance - $bidPrice;
-//                                 $sql = "UPDATE Users SET accountbalance=$remainingbalance WHERE userID = $buyerID"; 
-//                                 $result_user_update = $connection->query($sql); // should be true
-//                                 //get the seller's email address
-//                                 $sql = "SELECT email FROM Users WHERE userID = $sellerID"; 
-//                                 $result_seller=$connection->query($sql); 
-//                                 $row = $result_seller->fetch_assoc();
-//                                 $seller_email = $row["email"];
-//                                 // send email to notify seller too
-//                                 $subject = "Congratulations! Your auction for '$product_name' has finished!";
-//                                 $body = "Description: '$product_description'\n Awarded to: '$buyer_name'";
-//                                 $altbody = $body;
-//                                 send_to_email($seller_email, $subject, $body, $altbody, $emailee_name);
-//                                 // move the item away from product to archive
-//                                 $sql = "INSERT INTO Archive (
-//                                         productID,
-//                                         product_name, 
-//                                         product_description, 
-//                                         dealprice,
-//                                         quantity,
-//                                         categoryID, 
-//                                         conditionID, 
-//                                         buyerID, 
-//                                         sellerID,
-//                                         auctionable,
-//                                         dealdate
-//                                         ) VALUES (
-//                                         $productID,
-//                                         '$product_name', 
-//                                         '$product_description', 
-//                                         $dealprice,
-//                                         $quantity,
-//                                         $categoryID, 
-//                                         $conditionID, 
-//                                         $buyerID, 
-//                                         $sellerID,
-//                                         $auctionable,
-//                                         '$dealdate'
-//                                         )";
-//                                 $result_move = $connection->query($sql);
-//                                 // delete item from product
-//                                 $sql = "DELETE FROM Product WHERE productID=$productID";
-//                                 $result_del = $connection->query($sql);
-//                             } // else give to the next payer 
-//                         }
-//                     } // else give to the next payer
-//                 }
-//             } else {
-//                 $sql="UPDATE Product SET enddate='$newdate' WHERE productID=$productID";
-//                 $connection->query($sql);
-//             }
-//         }
-//     }
-// }
+                                // deduct balance from user
+                                $remainingbalance = $accountbalance - $bidPrice;
+                                $sql = "UPDATE Users SET accountbalance=$remainingbalance WHERE userID = $buyerID"; 
+                                $result_user_update = $connection->query($sql); // should be true
+                                //get the seller's email address
+                                $sql = "SELECT email FROM Users WHERE userID = $sellerID"; 
+                                $result_seller=$connection->query($sql); 
+                                $row = $result_seller->fetch_assoc();
+                                $seller_email = $row["email"];
+                                // send email to notify seller too
+                                $subject = "Congratulations! Your auction for '$product_name' has finished!";
+                                $body = "Description: '$product_description'\n Awarded to: '$buyer_name'";
+                                $altbody = $body;
+                                send_to_email($seller_email, $subject, $body, $altbody, $emailee_name);
+                                // move the item away from product to archive
+                                $sql = "INSERT INTO Archive (
+                                        productID,
+                                        product_name, 
+                                        product_description, 
+                                        dealprice,
+                                        quantity,
+                                        categoryID, 
+                                        conditionID, 
+                                        buyerID, 
+                                        sellerID,
+                                        auctionable,
+                                        dealdate
+                                        ) VALUES (
+                                        $productID,
+                                        '$product_name', 
+                                        '$product_description', 
+                                        $dealprice,
+                                        $quantity,
+                                        $categoryID, 
+                                        $conditionID, 
+                                        $buyerID, 
+                                        $sellerID,
+                                        $auctionable,
+                                        '$dealdate'
+                                        )";
+                                $result_move = $connection->query($sql);
+                                // delete item from product
+                                $sql = "DELETE FROM Product WHERE productID=$productID";
+                                $result_del = $connection->query($sql);
+                            } // else give to the next payer 
+                        }
+                    } // else give to the next payer
+                }
+            } else {
+                $sql="UPDATE Product SET enddate='$newdate' WHERE productID=$productID";
+                $connection->query($sql);
+            }
+        }
+    }
+}
 $connection->close();
 unset($_SESSION["expired_nonauction_listings"]);
 // unset($_SESSION["expired_auction_listings"]);
